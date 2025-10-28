332a3a4c2e3377d9c2167376c0774a99e22fa56d
Liz Tan <elizabethsztan@gmail.com>
2025-10-28 10:38:27 +0000

    added symbolicmodel

--- src/symtorch/__init__.py
+++ src/symtorch/__init__.py
@@ -3,9 +3,11 @@ Core SymTorch modules
 """
 
 from .SymbolicMLP import SymbolicMLP
+from .SymbolicModel import SymbolicModel
 from .toolkit import PruningMLP
 
 __all__ = [
     "SymbolicMLP",
+    "SymbolicModel",
     "PruningMLP"
 ]
