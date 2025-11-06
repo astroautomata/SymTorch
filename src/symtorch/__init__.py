9c705d9b9ad016112fbc49ffd785e13f4e686fa0
Liz Tan <elizabethsztan@gmail.com>
2025-11-06 15:13:25 +0000

    add my slime function

--- src/symtorch/__init__.py
+++ src/symtorch/__init__.py
@@ -5,9 +5,12 @@ Core SymTorch modules
 from .SymbolicMLP import SymbolicMLP
 from .SymbolicModel import SymbolicModel
 from .toolkit import PruningMLP
+from .SLIME import SLIME, regressor_to_function
 
 __all__ = [
     "SymbolicMLP",
     "SymbolicModel",
-    "PruningMLP"
+    "PruningMLP",
+    "SLIME",
+    "regressor_to_function"
 ]
