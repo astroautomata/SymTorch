a63aac3eddf57e48eed0c80301c9bde332112fb2
Liz Tan <elizabethsztan@gmail.com>
2025-11-07 18:15:11 +0000

    upload new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="torch-symbolic",
-    version="1.0.6",
+    version="1.1.0p",
     author="Liz Tan",
     author_email= "eszt2@cam.ac.uk",
     description="Deep Learning Interpretability with Symbolic Regression.",
