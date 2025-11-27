8bb8d20824e5337c4ae850e756c0d09251df7e91
Liz Tan <elizabethsztan@gmail.com>
2025-11-27 11:05:23 +0000

    added SymbolicModel

--- src/symtorch/__init__.py
+++ src/symtorch/__init__.py
@@ -3,13 +3,13 @@ Core SymTorch modules
 """
 
 from .SymbolicMLP import SymbolicMLP
-from .SymbolicModel_OLD import SymbolicModel
+from .SymbolicModel import SymbolicModel
 from .toolkit import PruningMLP
 from .SLIMEModel import SLIMEModel, regressor_to_function
 
 __all__ = [
     "SymbolicMLP",
-    "SymbolicModel_OLD",
+    "SymbolicModel",
     "PruningMLP",
     "SLIMEModel",
     "regressor_to_function"
